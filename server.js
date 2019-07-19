92f5f70b5cb12e76f90bfba23c797c3ef620e9e4
Vincent Teas <vincentteas@gmail.com>
2019-07-19 16:29:09 -0700

    able to connect to postgressSQL db

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const knex = require("knex");
+
+const postgres = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "cakemuffin",
+    database: "face-recognition-db"
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -38,16 +49,14 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(database.users[database.users.length - 1]);
+  const { email, name } = req.body;
+  postgres("users")
+    .returning("*")
+    .insert({ email: email, name: name, joined: new Date() })
+    .then(user => {
+      res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('Unable to register!'));
 });
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
