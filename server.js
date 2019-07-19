b591c79900fbdd509252585e1d8fb3fd0ee34fbc
Vincent Teas <vincentteas@gmail.com>
2019-07-19 09:57:39 -0700

    Update server.js
    retrieved registration info

--- server.js
+++ server.js
@@ -31,12 +31,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  console.log(
-    req.body.email,
-    database.users[0].email,
-    req.body.password,
-    database.users[0].password
-  );
   req.body.email === database.users[0].email &&
   req.body.password === database.users[0].password
     ? res.json("success")
@@ -49,7 +43,7 @@ app.post("/register", (req, res) => {
     id: "125",
     name: name,
     email: email,
-    passowrd: password,
+    password: password,
     entries: 0,
     joined: new Date()
   });
