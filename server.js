744a9be985ae96fd1c4117d9958b9bf19c8a9dd7
Vincent Teas <vincentteas@gmail.com>
2019-07-19 17:57:28 -0700

    added transaction for register

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const knex = require("knex");
+const bcrypt = require("bcrypt-nodejs");
 
 const postgres = knex({
   client: "pg",
@@ -49,13 +50,29 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { email, name } = req.body;
-  postgres("users")
-    .returning("*")
-    .insert({ email: email, name: name, joined: new Date() })
-    .then(user => {
-      res.json(user[0]);
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  postgres
+    .transaction(trx => {
+      trx
+        .insert({
+          hash: hash,
+          email: email
+        })
+        .into("login")
+        .returning("email")
+        .then(loginEmail => {
+          return trx("users")
+            .returning("*")
+            .insert({ email: loginEmail[0], name: name, joined: new Date() })
+            .then(user => {
+              res.json(user[0]);
+            });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
     })
+
     .catch(err => res.status(400).json("Unable to register!"));
 });
 app.get("/profile/:id", (req, res) => {
