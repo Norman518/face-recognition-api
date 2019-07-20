f5464db705b81b27bb3ef52b311e925a2817d535
Vincent Teas <vincentteas@gmail.com>
2019-07-20 13:36:39 -0700

    adding port

--- server.js
+++ server.js
@@ -1,36 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const postgres = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'cakemuffin',
-    database : 'face-recognition-db'
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "cakemuffin",
+    database: "face-recognition-db"
   }
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(postgres.users) })
-app.post('/signin', signin.handleSignin(postgres, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, postgres, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, postgres)})
-app.put('/image', (req, res) => { image.handleImage(req, res, postgres)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get("/", (req, res) => {
+  res.send(postgres.users);
+});
+app.post("/signin", signin.handleSignin(postgres, bcrypt));
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, postgres, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, postgres);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, postgres);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
